b670f7c2430bf454b5dd470cfd077d7cb0b34ef5
robocafaz <robocafaz@gmail.com>
2015-05-20 20:51:45 -0400

    Adding character view screen.

---
+++ TreCru.Web/Controllers/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TreCru.Web.Entity;
+using TreCru.Web.Services.Interfaces;
+
+namespace TreCru.Web.Controllers
+{
+    public class CharacterController : Controller
+    {
+        private readonly IRepository<Character> _characterRepo;
+
+        public CharacterController(IRepository<Character> characterRepo)
+        {
+            _characterRepo = characterRepo;
+        }
+
+        public ActionResult Index()
+        {
+            var allChars = _characterRepo.GetAll();
+            ViewBag.Title = "Characters";
+            return View(allChars);
+        }
+    }
+}
